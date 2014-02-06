1a7bd421135ddbd0c14b62a2c0db73f2e17fc36d
GJ <gjmcferrin@gmail.com>
2014-02-06 10:31:51 -0500

    [Bleeding] Replace setEntity(Entity) with setEntity(LivingEntity)

--- src/main/java/org/bukkit/event/entity/EntityTargetLivingEntityEvent.java
+++ src/main/java/org/bukkit/event/entity/EntityTargetLivingEntityEvent.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.LivingEntity;
 
 /**
  * Called when an Entity targets a {@link LivingEntity} and can only target
- * LivingEntity's.
+ * LivingEntities.
  */
 public class EntityTargetLivingEntityEvent extends EntityTargetEvent{
     public EntityTargetLivingEntityEvent(final Entity entity, final LivingEntity target, final TargetReason reason) {
@@ -17,18 +17,30 @@ public class EntityTargetLivingEntityEvent extends EntityTargetEvent{
     }
 
     /**
-     * Set the Entity that you want the mob to target.
+     * This method exists for legacy reasons to provide backwards
+     * compatibility. It will not exist at runtime and should not be used
+     * under any circumstances.
+     */
+    @Deprecated
+    public void _INVALID_setTarget(Entity target) {
+        if ((target == null) || (target instanceof LivingEntity)) {
+            super.setTarget(target);
+        }
+    }
+
+    /**
+     * Set the entity that you want the mob to target instead.
      * <p>
      * It is possible to be null, null will cause the entity to be
      * target-less.
      * <p>
-     * Must be a LivingEntity, or null.
+     * This is different from cancelling the event. Cancelling the event will
+     * cause the entity to keep an original target, while setting to be null
+     * will cause the entity to be reset.
      *
-     * @param target The entity to target
+     * @param target The LivingEntity to target
      */
-    public void setTarget(Entity target) {
-        if (target == null || target instanceof LivingEntity) {
-            super.setTarget(target);
-        }
+    public void setTarget(LivingEntity target) {
+        super.setTarget(target);
     }
 }
