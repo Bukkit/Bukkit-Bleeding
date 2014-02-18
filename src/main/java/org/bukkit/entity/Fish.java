8b46afed61521ef3b35e4e8b4d8e0f1da11bfd9d
GJ <gjmcferrin@gmail.com>
2014-02-18 15:01:59 -0500

    [Bleeding] Implement new fishing chance logic. Adds BUKKIT-5428
    Due to changes in fishing logic introduced in 1.7, the old getBiteChance()
    and setBiteChance(...) functions no longer work properly. This replaces both
    of these functions with replacements reflecting the new fishing logic.

--- src/main/java/org/bukkit/entity/Fish.java
+++ src/main/java/org/bukkit/entity/Fish.java
@@ -11,8 +11,10 @@ public interface Fish extends Projectile {
      * 0.0 = No Chance.<br>
      * 1.0 = Instant catch.
      *
-     * @return chance the bite chance
+     * @return the bite chance
+     * @deprecated No longer functions due to logic change in 1.7
      */
+    @Deprecated
     public double getBiteChance();
 
     /**
@@ -24,6 +26,22 @@ public interface Fish extends Projectile {
      * @param chance the bite chance
      * @throws IllegalArgumentException if the bite chance is not between 0
      *     and 1
+     * @deprecated No longer functions due to logic change in 1.7
      */
+    @Deprecated
     public void setBiteChance(double chance) throws IllegalArgumentException;
+
+    /**
+     * Get the number of ticks remaining before a fish will approach the hook.
+     *
+     * @return the number of ticks remaining before a fish approaches the hook.
+     */
+    public int getBiteTicks();
+
+    /**
+     * Set the number of ticks remaining before a fish approaches the hook.
+     *
+     * @param ticks the bite ticks
+     */
+    public void setBiteTicks(int ticks);
 }
